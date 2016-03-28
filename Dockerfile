3cab9a2b6ac487542e6d55b318f7b5ab5b9e269b
Thiago Figueiró <thiagocsf@gmail.com>
2016-03-28 12:17:54 +1100

    Move FLUME_CONF_DIR to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ RUN mkdir /opt/flume
 RUN wget -qO- http://archive.apache.org/dist/flume/1.6.0/apache-flume-1.6.0-bin.tar.gz \
   | tar zxvf - -C /opt/flume --strip 1
 
+ENV FLUME_CONF_DIR /opt/flume/conf
 ADD start-flume.sh /opt/flume/bin/start-flume
 
 ENV JAVA_HOME /opt/java
--- start-flume.sh
+++ start-flume.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-FLUME_CONF_DIR=${FLUME_CONF_DIR:-/opt/flume/conf}
-
 [[ -z "${FLUME_CONF_FILE}"  ]] && { echo "FLUME_CONF_FILE required";  exit 1; }
 [[ -z "${FLUME_AGENT_NAME}" ]] && { echo "FLUME_AGENT_NAME required"; exit 1; }
 
@@ -12,4 +10,3 @@ flume-ng agent \
   -f ${FLUME_CONF_FILE} \
   -n ${FLUME_AGENT_NAME} \
   -Dflume.root.logger=INFO,console
-
